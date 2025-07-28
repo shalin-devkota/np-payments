e336902d87b60d124ffe040d6e4edc96af044f6d
Shalin Devkota <shalin.devkota01@gmail.com>
2025-07-28 11:07:07 +0000

    fix: use full paths in init imports

--- np_payments/clients/esewa/__init__.py
+++ np_payments/clients/esewa/__init__.py
@@ -1,2 +1,2 @@
-from esewa_aysnc import AsyncEsewaClient
-from esewa import EsewaClient
+from np_payments.clients.esewa.esewa_aysnc import AsyncEsewaClient
+from np_payments.clients.esewa.esewa import EsewaClient
